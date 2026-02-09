36bad2b0ed27210caa783dad6acae41cab6fc1e9
fahriyafi <fahriyafi71@gmail.com>
2026-02-09 21:26:25 +0800

    Add files via upload

---
+++ aplikasiPerhitunganHarii/src/app/AplikasiPerhitunganHari.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app;
+import frame.frameApp;
+
+/**
+ *
+ * @author User
+ */
+public class AplikasiPerhitunganHari {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frameApp frame = new frameApp();
+        frame.setVisible(true);
+    }
+    
+}
